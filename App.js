701e67704bccdd74a3a45b52b09fbf573b950dd5
DiegoIacabo <logonrmlocal@fiap.com.br>
2024-04-23 09:59:06 -0300

    Aula de Rest

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import axios from 'axios';
+import React from 'react';
+import Counter from "react-native-counters";
 
 export default function App() {
+
+  const [activity, setActivity] = React.useState('');
+
+  const [participants, setParticipants] = React.useState(1);
+
+  const onPressButton = () => {
+    axios.get(`https://www.boredapi.com/api/activity?participants=${participants}`)
+    .then(response => {
+      console.log('resposta: ', response);
+      setActivity(response.data.activity)
+    })
+    .catch(e => {
+      console.log('error: ', e)
+    })
+    .finally(()=>{
+      console.log('finally')
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{activity}</Text>
+      <Counter start={1} max={5} onChange={(number)=>{setParticipants(number)}} />
+      <Button title='Nova Atividade' onPress={onPressButton}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +39,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
 });
